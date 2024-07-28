a3daae887e74494c1cef6a1f35a73d5ed4a6815c
Shogo Wada <aiz4338150@gmail.com>
2024-07-28 20:07:36 +0900

    feat: expressの初期設定

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import morgan from 'morgan';
+import 'express-async-errors';
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.get('/api/hello', (req, res) => {
+  res.json({
+    message: 'Hello Express',
+  });
+});
+
+app.get('/api/error', async () => {
+  throw new Error('Error endpoint');
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+function errorHandler(err: any, _req: express.Request, res: express.Response, _next: express.NextFunction) {
+  console.error('Unexpected error occurred', err);
+  res.status(500).send({
+    message: 'Unexpected error occurred',
+  });
+}
